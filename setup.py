e9fe61ee749f9eb7b9dcf0fd0180a51b1a7a2ae6
Nils Måsén <nils@piksel.se>
2021-02-12 12:00:43 +0100

    fix: update dependencies and add missing package

--- setup.py
+++ setup.py
@@ -20,5 +20,5 @@ setup(
     app=APP,
     data_files=DATA_FILES,
     options={'py2app': OPTIONS},
-    setup_requires=['py2app'], install_requires=['barcode', 'PyQt5', 'django-qrcode', 'serial']
+    setup_requires=['py2app'], install_requires=['PyQt5', 'django-qrcode', 'pyserial', 'packbits', 'pypng', 'appdirs']
 )
