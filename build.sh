4fc7251fe51b700f14fcb4d5a91e0d663e4ab139
nils måsén <nils@piksel.se>
2022-08-07 14:37:29 +0200

    fix build script path

--- build.sh
+++ build.sh
@@ -1,11 +1,15 @@
 
-APP_SCRIPT='src/pytouch-cube/__main__.py'
+APP_SCRIPT='src/pytouch_cube/__main__.py'
 ICON='pytouch3.ico'
 IMG_DATA="pytouch3.png:."
 
-VER=$(git describe)
+VER=$(git describe --all)
 
 echo "APP_VERSION = '$VER'" > src/pytouch_cube/version.py
+
+echo "version.py:"
+cat src/pytouch_cube/version.py
+echo
 
 pyinstaller \
     --add-data="$IMG_DATA" \
